b6358383ec221f3622797a960d41d8ac1ba83583
datben <benjamindumont@live.com>
2021-06-10 10:49:29 +0200

    v1

--- elevator.c
+++ elevator.c
@@ -1,1 +1,36 @@
 #include "elevator.h"
+#include "person.h"
+#include <stdlib.h>
+
+PersonList *exitElevator(Elevator *e)
+{
+    int currentfloor = (e->currentFloor);
+    PersonList *l = (e->persons);
+    while (l)
+    {
+        if (((l->person)->dest) = currentfloor)
+        {
+            l = l->next;
+        }
+    }
+    return l;
+}
+
+PersonList *enterElevator(Elevator *e, PersonList *waitingList)
+{
+    PersonList *h = (PersonList *)malloc(sizeof(PersonList));
+    int currentfloor = (e->currentFloor);
+    while (waitingList && (len(e->persons) < (e->capacity)))
+    {
+        if (((waitingList->person)->dest) = currentfloor)
+        {
+            e->persons = insert(h->person, e->persons);
+            waitingList = waitingList->next;
+        }
+        else
+        {
+            h = insert(waitingList->person, h);
+        }
+    }
+    return h;
+}
